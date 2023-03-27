9b677fbf7e31777dc1adbdf2bbb15fa90ab4025f
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2023-03-27 12:53:58 -0300

    fazendo pedido 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,9 @@
+export default abstract class Race {
+  constructor(readonly name:string, readonly dexterity:number) {}
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints():number;
+}
